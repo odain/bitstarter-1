7d6111c70c5e4ed582d1a87088cecc2276a96672
ODA <example@stanford.edu>
2013-07-10 22:47:06 +0000

    path

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-fs.readFile('index.html', function (err, data) {
+fs.readFile('./index.html', function (err, data) {
   if (err) throw err;
   var buffer = new Buffer(256); 
   var len = buf.write(data,0);
