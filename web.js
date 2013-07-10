3f19d64ab2e57058b27d9272dbefe29afdae9728
ODA <example@stanford.edu>
2013-07-10 22:58:43 +0000

    buf var

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 fs.readFile('index.html', function (err, data) {
   if (err) throw err;
-  var buffer = new Buffer(256); 
+  var buf = new Buffer(256); 
   var len = buf.write(data,0);
   response.send(buf.toString('utf8', 0, len));
   //response.send(data);
