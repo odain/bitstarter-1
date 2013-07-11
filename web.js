e51fe85a883a3d694c0d0108c516cea402a19208
ODA <example@stanford.edu>
2013-07-11 00:00:57 +0000

    readfile

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
 fs.readFile('./index.html', function (err, data) {
   if (err) throw err;
 //response.send("youpi");
-response.send(data);
+response.send(data.toString('utf-8'));
 });
 //  response.send("toto");
 });
