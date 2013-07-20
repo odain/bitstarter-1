db315e9eff31aa8824fe19366cdf0ccb29846e1c
Ubuntu <ubuntu@ip-172-31-11-50.us-west-2.compute.internal>
2013-07-20 21:02:33 +0000

    HW4_CSS

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ response.send(data.toString('utf-8'));
 });
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
